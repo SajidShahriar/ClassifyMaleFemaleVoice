5f1993c592792445cb9932f20609f09d0414f789
SajidShahriar <56547005+SajidShahriar@users.noreply.github.com>
2020-07-23 18:27:10 +0600

    Create voice.py

---
+++ voice.py
@@ -0,0 +1,31 @@
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn import naive_bayes
+
+voice=pd.read_csv('voice.csv')
+le = preprocessing.LabelEncoder()
+voice["label"] = le.fit_transform(voice["label"])
+
+voice[:]=preprocessing.MinMaxScaler().fit_transform(voice)
+
+train, test = train_test_split(voice, test_size=0.3)
+x_train = train.iloc[:, :-1]
+y_train = train["label"]
+x_test = test.iloc[:, :-1]
+y_test = test["label"]
+
+x_train3 = train[["meanfun","IQR","Q25"]]
+y_train3 = train["label"]
+x_test3 = test[["meanfun","IQR","Q25"]]
+y_test3 = test["label"]
+
+def classify(model,x_train,y_train,x_test,y_test):
+    from sklearn.metrics import classification_report
+    target_names = ['female', 'male']
+    model.fit(x_train,y_train)
+    y_pred=model.predict(x_test)
+    print(classification_report(y_test, y_pred, target_names=target_names, digits=2))
+
+model=naive_bayes.GaussianNB()
+classify(model,x_train,y_train,x_test,y_test)
