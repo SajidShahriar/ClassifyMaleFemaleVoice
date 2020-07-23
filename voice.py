b62c7a3d09451fba9d00d0067afcd29410d3c314
SajidShahriar <56547005+SajidShahriar@users.noreply.github.com>
2020-07-23 22:16:25 +0600

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,19 +1,36 @@
+import numpy as np 
 import pandas as pd 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn import naive_bayes
+#from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 voice=pd.read_csv('voice.csv')
 le = preprocessing.LabelEncoder()
 voice["label"] = le.fit_transform(voice["label"])
 
 voice[:]=preprocessing.MinMaxScaler().fit_transform(voice)
-
 train, test = train_test_split(voice, test_size=0.3)
 x_train = train.iloc[:, :-1]
 y_train = train["label"]
 x_test = test.iloc[:, :-1]
 y_test = test["label"]
+
+x_train3 = train[["meanfun","IQR","Q25"]]
+y_train3 = train["label"]
+x_test3 = test[["meanfun","IQR","Q25"]]
+y_test3 = test["label"]
 
 classifiers = [
     KNeighborsClassifier(3),
@@ -38,9 +55,8 @@ def classify(model,x_train,y_train,x_test,y_test):
     return dt_entry
 
 for clf in classifiers:
-    dt= dt.append(classify(clf,x_train,y_train,x_test,y_test))
+    dt= dt.append(classify(clf,x_train3,y_train3,x_test3,y_test3))
+    dt = dt.sort_values('Accuracy',ascending=False)
 
-plt.figure(figsize=(5,5))
-sns.barplot(x='Accuracy', y='Classifier', data=dt)
-
+print(dt)
 
